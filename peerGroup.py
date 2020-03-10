3d414b16a6c349d92825e6b898e3ae6b5f81051a
Selina Isak <Selina.Isak@gmx.at>
2020-03-10 13:05:58 +0100

    extracted printing of address-assignment to method printInfo()

--- peerGroup.py
+++ peerGroup.py
@@ -12,15 +12,19 @@ class peerGroup:
             others = addresses[:]
             hostport = int(others[i].split(':')[1])
             del others[i]
-            print("Others: " + str(others))
+            #print("Others: " + str(others))
             self.peers.append(Peer(n-1, hostport, others ))
+
+    def printInfo(self):
+        for i in range(len(self.peers)):
+            print("[PEER %d]: " % i)
+            print("\tServer: %d" % self.peers[i].server.port)
+            for client in self.peers[i].clients:
+                print("\tClient %d: %s" % (client.id, client.address))
 
 if __name__ == '__main__':
     myGroup = peerGroup(4, ["localhost:5556", "localhost:5557", "localhost:5558", "localhost:5559"])
-    for i in range(len(myGroup.peers)):
-        print("[PEER %d]: " %i)
-        print("\tServer: %d" %myGroup.peers[i].server.port)
-        for client in myGroup.peers[i].clients:
-            print("\tClient %d: %s" %(client.id, client.address))
+    myGroup.printInfo()
+
 
 
