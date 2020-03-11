e9bf4a8c6c304df5358a9f9157d991e4911da12b
Selina Isak <Selina.Isak@gmx.at>
2020-03-11 13:48:16 +0100

    Server reads lines of file for 10sec, closes file and shuts down

--- peers_seperate/server.py
+++ peers_seperate/server.py
@@ -23,15 +23,17 @@ class Server (threading.Thread):
 
     def run(self):
         count = 0
+        logfile = open("ChunkChanges-very-distributed.csv", "r")
+        line = logfile.readline()
+
         while count < 20:
-            zipcode = self.port
-            temperature = randrange(-80, 135)
-            relhumidity = randrange(10, 60)
-            updateString = str(zipcode) + " " + str(temperature) + " " + str(relhumidity)
-            self.socket.send_string(updateString)
-            print("[localhost:%d]: sent update %s" %(self.port, updateString))
+            self.socket.send_string(str(self.port) + " " +line)
+            print("[localhost:%d]: sent update %d %s" %(self.port, self.port, line))
+            line = logfile.readline()
             count += 1
             time.sleep(0.5)  # set to 0.5
+
+        logfile.close()
 
         if(self.peer != None):
             print("[localhost:%d]: SERVER shutting down ... " %(self.port))
