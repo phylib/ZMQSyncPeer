14048d6bf9951e241c2766defec92b0a677d567f
selisak <Selina.Isak@aau.at>
2020-03-24 11:09:25 +0100

    the logger locks the file if a threads wants to write to it and releases the lock after the write operation

--- peers_seperate/logging/logger.py
+++ peers_seperate/logging/logger.py
@@ -1,20 +1,19 @@
-import os
-
+import threading
 class Logger:
     def __init__(self, logDir):
         self.logDir = logDir
         self.logFile = open(logDir+"\\changeLog.txt", "w");
-
-    '''def initFile(self):
-        self.logFile.write("Hello there!\n");
-        print("Logfile-Path: %s" %os.path.abspath(self.logFile.name))
-        self.logFile.close()'''
+        self.lock = threading.Lock();
 
     def logChunkUpdateProduced(self, chunk, timestamp):
+        self.lock.acquire()
         self.logFile.write("%s\tOUT\t%d\t%d\t%d\n" %(timestamp, chunk.x, chunk.y, chunk.data))
+        self.lock.release()
 
     def logChunkUpdateReceived(self, chunk, timestamp):
+        self.lock.acquire()
         self.logFile.write("%s\tIN\t%d\t%d\t%d\n" %(timestamp, chunk.x, chunk.y, chunk.data))
+        self.lock.release()
 
     def closeFile(self):
         self.logFile.close()
