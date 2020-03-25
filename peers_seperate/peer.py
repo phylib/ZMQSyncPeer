0302ea8cd8063867414b5ac1a753c874121468ef
selisak <Selina.Isak@aau.at>
2020-03-25 17:28:10 +0100

    Docstrings are added in peer.py

--- peers_seperate/peer.py
+++ peers_seperate/peer.py
@@ -1,9 +1,3 @@
-#
-#   Peer: consisting of 1 server & n clients
-#   calls server + clients as threads
-#   shuts clients down, when server wants to end the connection
-#
-
 from client import Client
 from server import Server
 from logging.logger import Logger
@@ -11,10 +5,35 @@ import os
 import argparse
 
 class Peer:
+    """
+    The Peer class starts a server-thread and n client-threads
+    where each client is subscribed to the server.
+
+    Each peer consists of one server, one logger and n clients. The server
+    as well as the clients are implemented as threads and started in the peer.
+    All clients are subscribed to the server.
+    When the peer is started via commandline several arguments are
+    passed to it. These arguments are then passed to the server, the logger and
+    the clients.
+
+    """
 
     def __init__(self, hostport, others, coordinates, tracefile, logDir, testing):
-        # 1 publisher
-        # n subscribers
+        """
+           Initializes the peer.
+           :param hostport: specifies the port number on which the server broadcasts its updates
+           :type hostport: int
+           :param others: the IP-addresses and the port number of the clients in the form IP-address:port number
+           :type others: str
+           :param coordinates: defines the rectangle to observe in the form x1,y1,x2,y2
+           :type coordinates: str
+           :param tracefile: the path to the tracefile which the server reads from
+           :type tracefile: str
+           :param logDir: the path to the directory where the logfile should be saved
+           :type logDir: str
+           :param testing: if true the server only reads 20 lines from the tracefile, else it reads everything
+           :type testing: bool
+        """
         self.logger = Logger(logDir);
         self.clients = []
         self.server = Server(hostport, coordinates, tracefile, testing, self)
@@ -31,12 +50,22 @@ class Peer:
             client.join()
 
     def shutdown(self):
+        """
+        This method is called when the server shuts down.
+        The peer then shuts all clients down and tells
+        the logger to close the logfile.
+        """
         for client in self.clients:
             client.shutdown()
         self.logger.closeFile()
 
 if __name__ == "__main__":
-
+    """
+    This method is called when the peer is 
+    started via the commandline.
+    Here the necessary arguments are parsed 
+    and passed to the peer.
+    """
     parser = argparse.ArgumentParser(description='Start a peer with one server and several clients.')
 
     parser.add_argument('--serverPort', type=int,
@@ -60,13 +89,10 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    # --serverPort=5556
-    # --addresses="localhost:5557, localhost:5558, localhost:5559"
-    #log the params in file
-
     if(not(os.path.exists(args.tracefile))):
         print("Path to tracefile does not exist!\n")
     else:
+        # log the params in file
         paramsLog = open("paramsLog.txt", "w");
         paramsLog.write("--serverPort=%d\n--clients=%s\n--coordinates=%s\n--logDir=%s"
                     %(args.serverPort, args.clients, args.coordinates, args.logDir));
